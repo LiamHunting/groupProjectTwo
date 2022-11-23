25bd67547b14386f8627317f485a32ef11fb6402
123Lizzie <123lizziehunt@gmail.com>
2022-11-22 20:43:09 -0700

    Checkout functionality test added

--- swagLabs.test.ts
+++ swagLabs.test.ts
@@ -29,4 +29,22 @@ describe("Test for SwagLabs Webpage", () => {
     test("login method functionality", async () => {
         await swagP.login();
     });
+    test("Checkout functionality", async () => {
+        await swagP.login();
+        await driver.findElement(swagP.backpackATC).click();
+        await driver.findElement(swagP.shoppingCart).click();
+        await driver.findElement(swagP.checkoutBtn).click();
+        await driver.findElement(swagP.firstNameInput).click();
+        await driver.findElement(swagP.firstNameInput).clear();
+        await driver.findElement(swagP.firstNameInput).sendKeys("Test");
+        await driver.findElement(swagP.lastNameInput).click();
+        await driver.findElement(swagP.lastNameInput).clear();
+        await driver.findElement(swagP.lastNameInput).sendKeys("Name");
+        await driver.findElement(swagP.zipInput).click();
+        await driver.findElement(swagP.zipInput).clear();
+        await driver.findElement(swagP.zipInput).sendKeys("12345");
+        await driver.findElement(swagP.continuechkBtn).click();
+        await driver.findElement(swagP.finishBtn).click();
+        await driver.findElement(swagP.backHomeBtn).click();
+    });
 });
